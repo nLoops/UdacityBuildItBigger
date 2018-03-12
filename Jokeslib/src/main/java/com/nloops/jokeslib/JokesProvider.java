b14cfdcea9fa8a91de9a8981270581180c4c3598
Ahmed Ibrahim <as01220767994@gmail.com>
2018-03-12 23:48:56 +0300

    feat : add module Java Library "Jokes Provider "

---
+++ Jokeslib/src/main/java/com/nloops/jokeslib/JokesProvider.java
@@ -0,0 +1,11 @@
+package com.nloops.jokeslib;
+
+/**
+ * This class will act like a provider of jokes to make it easy to provide android app with jokes
+ */
+public class JokesProvider {
+
+    public static String getJoke() {
+        return "Hello from Jokes Library";
+    }
+}
